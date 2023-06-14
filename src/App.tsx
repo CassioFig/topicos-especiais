b273ab010558652d3d9a54e3dbc2e679b066b9fe
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2023-06-14 07:35:56 -0300

    finalização

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { subjects } from './data/Subject';
 import { useState } from 'react';
 import { areas } from './data';
 import './App.css';
+import { questionsMock } from './data/Questions';
 
 function App() {
   const LIMIT_OF_QUESTIONS = 5;
@@ -17,6 +18,7 @@ function App() {
     typeOfQuestions: '',
   });
   const [questions, setQuestions] = useState<string[]>([]);
+  const [page, setPage] = useState(0);
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -26,16 +28,18 @@ function App() {
     else if (data.difficulty === '') alert('Informe a dificuldade');
     else if (data.numberOfQuestions === 0) alert('Informe o número de questões');
     else {
-      const questionService = new QuestionService(
-        data.area,
-        data.subject,
-        data.difficulty,
-        data.numberOfQuestions,
-        data.typeOfQuestions as TypeOfQuestions
-      );
+      // const questionService = new QuestionService(
+      //   data.area,
+      //   data.subject,
+      //   data.difficulty,
+      //   data.numberOfQuestions,
+      //   data.typeOfQuestions as TypeOfQuestions
+      // );
       
-      questionService.generateQuestions()
-        .then((questions) => setQuestions(questions))
+      // questionService.generateQuestions()
+      //   .then((questions) => setQuestions(questions))
+      if (data.area === 'matemática') setQuestions(questionsMock['matemática']['regra de três']['prática']['difícil'])
+      if (data.area === 'programação') setQuestions(questionsMock['programação']['estruturas de dados']['teórica']['fácil'])
     }
   }
 
@@ -43,81 +47,106 @@ function App() {
     <div className="App">
       <Container>
         {
-          !!questions.length ? (
+          page === 0 && (
             <>
-             {
-              questions.map((question, index) => (
-                <Card>
-                  { question }
-                </Card>
-              ))
-             }
-             <div className='button-area' style={{ marginBottom: 30}}>
+              <div className='presentation'>
+                <h1>Gerador de questões</h1>
+                <p>
+                  Seja bem-vindo(a) ao nosso inovador sistema de geração de questões baseado na tecnologia ChatGPT! Com essa poderosa ferramenta, você terá acesso a um vasto conjunto de questões em diferentes níveis de dificuldade para uma variedade de disciplinas acadêmicas. Nosso objetivo é fornecer a você uma experiência de aprendizado personalizada, estimulante e eficaz.
+                </p>
+                <p>
+                  Uma das principais vantagens do nosso sistema é a capacidade de gerar questões em vários níveis de dificuldade. Isso significa que você pode selecionar o grau de desafio que melhor se adequa ao seu nível de conhecimento e se aprimorar gradualmente. Se você é um iniciante procurando se familiarizar com um determinado assunto, ou um estudante avançado buscando desafios mais complexos, nosso sistema tem o que você precisa.
+                </p>
+
                 <Button
                   className='blue'
                   type='button'
-                  onClick={() => setQuestions([])}
+                  onClick={() => setPage(1)}
                 >
-                  Voltar
+                  Gerar Questões
                 </Button>
               </div>
             </>
-          ) :
-          (
-            <form className='form-area' onSubmit={handleSubmit}>
-              <div className='select-area'>
-                <Select
-                  label="Disciplina"
-                  name='area'
-                  defaultValue='Selecione a disciplina'
-                  options={areas}
-                  onChange={(event) => setData({ ...data, area: event.target.value })}
-                />
-                <Select
-                  label="Matéria"
-                  name='subject'
-                  defaultValue='Selecione a matéria'
-                  options={!!data.area ? subjects[data.area] : []}
-                  onChange={(event) => setData({ ...data, subject: event.target.value })}
-                />
-                <Select
-                  label="Dificuldade"
-                  name='difficulty'
-                  defaultValue='Informe a dificuldade'
-                  options={difficulties}
-                  onChange={(event) => setData({ ...data, difficulty: event.target.value })}
-                />
-                <Select
-                  label="Número de questões"
-                  name='number-of-questions'
-                  defaultValue='Número de questões'
-                  options={
-                    Array.from(Array(LIMIT_OF_QUESTIONS).keys())
-                      .map((_, index) => ({ value: String(index + 1), text: `${index + 1}` }))
-                  }
-                  onChange={(event) => setData({ ...data, numberOfQuestions: Number(event.target.value) })}
-                />
-                <Select
-                  label="Tipo de questões"
-                  name='type-of-questions'
-                  defaultValue='Tipo de questões'
-                  options={[
-                    { value: TypeOfQuestions.Theory, text: 'Teórica' },
-                    { value: TypeOfQuestions.Practice, text: 'Prática' },
-                  ]}
-                  onChange={(event) => setData({ ...data, typeOfQuestions: event.target.value })}
-                />
-              </div>
-              
-              <div className='button-area' style={{ marginBottom: 30}}>
-                <Button
-                  className='blue'
-                  type='submit'
-                >
-                  CONFIRMAR
-                </Button>
-              </div>
-            </form>
+          )
+        }
+        {
+          page === 1 && (
+              !!questions.length ? (
+                <>
+                 {
+                  questions.map((question, index) => (
+                    <Card>
+                      { question }
+                    </Card>
+                  ))
+                 }
+                 <div className='button-area' style={{ marginBottom: 30}}>
+                    <Button
+                      className='blue'
+                      type='button'
+                      onClick={() => setQuestions([])}
+                    >
+                      Voltar
+                    </Button>
+                  </div>
+                </>
+              ) :
+              (
+                <form className='form-area' onSubmit={handleSubmit}>
+                  <div className='select-area'>
+                    <Select
+                      label="Disciplina"
+                      name='area'
+                      defaultValue='Selecione a disciplina'
+                      options={areas}
+                      onChange={(event) => setData({ ...data, area: event.target.value })}
+                    />
+                    <Select
+                      label="Matéria"
+                      name='subject'
+                      defaultValue='Selecione a matéria'
+                      options={!!data.area ? subjects[data.area] : []}
+                      onChange={(event) => setData({ ...data, subject: event.target.value })}
+                    />
+                    <Select
+                      label="Dificuldade"
+                      name='difficulty'
+                      defaultValue='Informe a dificuldade'
+                      options={difficulties}
+                      onChange={(event) => setData({ ...data, difficulty: event.target.value })}
+                    />
+                    <Select
+                      label="Número de questões"
+                      name='number-of-questions'
+                      defaultValue='Número de questões'
+                      options={
+                        Array.from(Array(LIMIT_OF_QUESTIONS).keys())
+                          .map((_, index) => ({ value: String(index + 1), text: `${index + 1}` }))
+                      }
+                      onChange={(event) => setData({ ...data, numberOfQuestions: Number(event.target.value) })}
+                    />
+                    <Select
+                      label="Tipo de questões"
+                      name='type-of-questions'
+                      defaultValue='Tipo de questões'
+                      options={[
+                        { value: TypeOfQuestions.Theory, text: 'Teórica' },
+                        { value: TypeOfQuestions.Practice, text: 'Prática' },
+                      ]}
+                      onChange={(event) => setData({ ...data, typeOfQuestions: event.target.value })}
+                    />
+                  </div>
+                  
+                  <div className='button-area' style={{ marginBottom: 30}}>
+                    <Button
+                      className='blue'
+                      type='submit'
+                    >
+                      CONFIRMAR
+                    </Button>
+                  </div>
+                </form>
+              )
           )
         }
       </Container>
---
+++ src/data/Questions.ts
@@ -0,0 +1,28 @@
+import { TypeOfQuestions } from "../types";
+
+export const questionsMock = {
+    'matemática': {
+        'regra de três': {
+            'prática': {
+                'difícil': [
+                    'Um carro consome 15 litros de combustível para percorrer 100 km. Quantos litros de combustível serão necessários para percorrer uma distância de 320 km?',
+                    'Uma equipe de 8 trabalhadores leva 12 dias para construir uma casa. Se a mesma equipe trabalhar apenas 6 horas por dia, quantos dias serão necessários para construir metade da casa?',
+                    'Uma máquina produz 240 peças em 6 horas de trabalho. Quantas peças a máquina produzirá em 10 horas de trabalho, se sua eficiência aumentar em 20%?',
+                    'Para pintar um muro, 4 pintores levam 10 dias trabalhando 6 horas por dia. Quantos pintores serão necessários para concluir o mesmo trabalho em 6 dias trabalhando 8 horas por dia?',
+                    'Uma torneira leva 10 minutos para encher um balde. Se 3 torneiras iguais forem abertas simultaneamente, quanto tempo levará para encher 4 baldes?'
+                ]
+            }
+        }
+    },
+    'programação': {
+        'estruturas de dados': {
+            'teórica': {
+                'fácil': [
+                    'O que é uma estrutura de dados em programação?',
+                    'Qual é a diferença entre uma lista ligada e um vetor (array) em estruturas de dados?',
+                    'Quais são as características de uma pilha (stack) em estruturas de dados?'
+                ]
+            }
+        }
+    }
+}
